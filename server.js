bc2dbafcc668ec06a89ad880c6af73b1676296ad
Juan Tipismana <juan.tipismana@bbva.com>
2018-09-05 20:34:30 -0500

    v1

---
+++ server.js
@@ -0,0 +1,17 @@
+const express= require('express'),
+	 path = require('path');
+
+const app =express();
+
+
+app.use(express.static('./dist/schematics-app'));
+
+app.get('/*', (req,res)=>{
+    
+res.sendFile(path.join(__dirname,'/dist/schematics-app/index.html'));
+
+});
+
+app.listen(process.env.PORT || 8080, ()=>{
+console.log('Server started');
+})
